79f4746fdf1fb3be3039ed134e5a92c06de8d57e
Seulgi Kim <dev@seulgi.kim>
2025-01-29 02:44:23 +0900

    Highlights selected apples

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ function draw_game(main: HTMLDivElement) {
     box.id = "box";
     game.appendChild(box);
 
+    const apples: HTMLDivElement[] = [];
     for (let i = 0; i < 10; i += 1) {
         const row = document.createElement("div");
         for (let j = 0 ; j < 17; j += 1) {
@@ -31,6 +32,7 @@ function draw_game(main: HTMLDivElement) {
             const value = Math.ceil(Math.random() * 9); // 1 ~ 9;
             apple.innerText = `${value}`;
             row.appendChild(apple);
+            apples.push(apple);
         }
         box.appendChild(row);
     }
@@ -53,25 +55,31 @@ function draw_game(main: HTMLDivElement) {
             return;
         }
         selection_box.style.display = "";
-        selection_box.style.width = `${Math.abs(e.clientX - start_point.x)}px`;
-        selection_box.style.height = `${Math.abs(e.clientY - start_point.y)}px`;
-        if (e.clientX < start_point.x) {
-            selection_box.style.left = `${e.clientX}px`;
-        } else {
-            selection_box.style.left = `${start_point.x}px`;
-        }
-        if (e.clientY < start_point.y) {
-            selection_box.style.top = `${e.clientY}px`;
-        } else {
-            selection_box.style.top = `${start_point.y}px`;
-        }
-        // TODO: highlight selected apples
+        const width = Math.abs(e.clientX - start_point.x);
+        const height = Math.abs(e.clientY - start_point.y);
+        selection_box.style.width = `${width}px`;
+        selection_box.style.height = `${height}px`;
+
+        const left = e.clientX < start_point.x ? e.clientX : start_point.x;
+        const top = e.clientY < start_point.y ? e.clientY : start_point.y;
+        selection_box.style.left = `${left}px`;
+        selection_box.style.top = `${top}px`;
+
+        apples.forEach(apple => {
+           if (isSelectedApple(apple, {left, top, right: left + width, bottom: top + height})) {
+               apple.classList.add("selected");
+           } else {
+               apple.classList.remove("selected");
+           }
+        });
     });
     game.addEventListener("mouseup", e => {
         e.preventDefault();
         try {
-            // TODO: reset apple highlights
             // TODO: select apples
+            apples.forEach(apple => {
+                apple.classList.remove("selected");
+            });
         } finally {
             start_point = null;
             selection_box.style.display = "none";
@@ -80,6 +88,16 @@ function draw_game(main: HTMLDivElement) {
 
     main.innerHTML = "";
     main.append(game);
+}
+
+function isSelectedApple(apple: HTMLDivElement, selected: {left: number; right: number; top: number; bottom: number}): boolean {
+    const rect = apple.getBoundingClientRect();
+    const x = (rect.left + rect.right) / 2;
+    if (!(selected.left <= x && x <= selected.right)) {
+        return false;
+    }
+    const y = (rect.top + rect.bottom) / 2;
+    return selected.top <= y && y <= selected.bottom;
 }
 
 window.addEventListener("load", () => {
