489c30950d1cf956172991799d76dc3fe55503c3
Seulgi Kim <dev@seulgi.kim>
2025-01-30 15:47:15 +0900

    Add mute button

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,11 @@ window.addEventListener("load", () => {
     const main = document.querySelector("#main")! as HTMLDivElement;
     const controller = document.querySelector("#controller")! as HTMLDivElement;
     draw_menu(main, controller);
+
+    const bite_effect = document.querySelector("#bite-effect")! as HTMLAudioElement;
+    const mute = document.querySelector("#mute")! as HTMLInputElement;
+    mute.addEventListener("change", e => {
+        const target = e.target as HTMLInputElement;
+        bite_effect.muted = target.checked;
+    });
 });
